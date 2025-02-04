e5fc6c3b151320ba1d24753438eb0952904890be
Jaime <jaimettb@gmail.com>
2025-02-04 19:23:04 -0300

    Reduce array

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -255,3 +255,6 @@ const adventureBooks = books
   .filter((book) => book.genres.includes("adventure"))
   .map((book) => book.title);
 adventureBooks;
+
+const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
+pagesAllBooks;
