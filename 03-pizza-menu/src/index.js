8b25ddd3403742ac7ce61f85d123d5bfaa83d01c
Jaime <jaimettb@gmail.com>
2025-02-17 18:29:00 -0300

    Conditional returning

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -108,6 +108,10 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
 
+  if (props.pizzaObj.soldOut) {
+    return null;
+  }
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -123,12 +127,16 @@ function Pizza(props) {
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
-  const closeHour = 22;
+  const closeHour = 12;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
   // else alert("Sorry we're closed");
+
+  // if (!isOpen) {
+  //   return <p>CLOSED</p>;
+  // }
 
   return (
     <footer className="footer">
