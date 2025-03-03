03ff63e8b239655cae072a1a9219c0bbfffe4ef2
Jaime <jaimettb@gmail.com>
2025-03-03 10:56:13 -0300

    React fragment

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -72,6 +72,7 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+  //const pizzas = [];
   const numPizzas = pizzas.length;
 
   return (
@@ -79,11 +80,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone even, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working in our menu, pleace come later :)</p>
       )}
