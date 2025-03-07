c5b4c650de8a940322031d495790a72dbd78fb3f
Jaime <jaimettb@gmail.com>
2025-03-06 22:06:13 -0300

    Steps challenge final version

--- steps-challenge/src/App.js
+++ steps-challenge/src/App.js
@@ -2,84 +2,39 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
-
-  const handlePrevious = function handlePrevious() {
-    setStep((s) => s + 1);
-  };
-
-  const handleNext = function handlePrevious() {
-    setStep((s) => s - 1);
-  };
-
-  const handlePreviousCount = function handlePrevious() {
-    setCount((c) => c + 1);
-  };
-
-  const handleNextCount = function handlePrevious() {
-    setCount((c) => c - 1);
-  };
-
   return (
     <div className="App">
-      <StepLine
-        handleNext={handleNext}
-        handlePrevious={handlePrevious}
-        step={step}
-      />
-      <CountLine
-        handleNext={handleNextCount}
-        handlePrevious={handlePreviousCount}
-        count={count}
-      />
-      <LabelLine step={step} count={count} />
+      <Counter />
     </div>
   );
 }
 
-function StepLine({ handleNext, handlePrevious, step }) {
-  return (
-    <div>
-      <button onClick={handleNext}>-</button>
-      Step: {step}
-      <button onClick={handlePrevious}>+</button>
-    </div>
-  );
-}
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
 
-function CountLine({ handleNext, handlePrevious, count }) {
-  return (
-    <div>
-      <button onClick={handleNext}>-</button>
-      Count: {count}
-      <button onClick={handlePrevious}>+</button>
-    </div>
-  );
-}
-
-function LabelLine({ step, count }) {
-  const dateIncrement = count * step;
-
-  console.log(dateIncrement);
-
-  const date = new Date();
-  const updatedDate = date.setDate(date.getDate() + dateIncrement);
-  const formattedDate = new Date(updatedDate).toLocaleDateString("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
-
-  console.log(formattedDate);
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
 
   return (
     <div>
-      {dateIncrement === 0
-        ? "Today is"
-        : `${dateIncrement} days from today is `}{" "}
-      {formattedDate}
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>+</button>
+        <span> Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>+</button>
+        <span> Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <span>
+        {count === 0
+          ? "Today is "
+          : count > 0
+          ? `${count} days from today is`
+          : `${count} days ago was`}
+      </span>
     </div>
   );
 }
