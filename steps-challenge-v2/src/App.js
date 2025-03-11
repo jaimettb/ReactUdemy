aa829da784f1611bbdaae7aec6eb0baad3ed5d6e
Jaime <jaimettb@gmail.com>
2025-03-10 21:20:01 -0300

    Setep challenge v2

---
+++ steps-challenge-v2/src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <div>
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
+          <span>Step: {step}</span>
+        </div>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>+</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <span>
+        {count === 0
+          ? "Today is "
+          : count > 0
+          ? `${count} days from today is`
+          : `${count} days ago was`}
+        {date.toDateString()}
+      </span>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
