c2def6850817afe7bd45e14c4faf2881e1de5fd7
Jaime <jaimettb@gmail.com>
2025-03-15 15:03:16 -0300

    Derived state

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Status />
+      <Status items={items} />
     </div>
   );
 }
@@ -112,10 +112,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Status() {
+function Status({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🛄 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everithing! Ready to go ✈️"
+          : `🛄 You have ${numItems} items on your list, and you already packed${" "}
+          ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
