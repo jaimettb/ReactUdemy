ebfd44e8d45e3a5a199480b7ca1018b4caba20f3
Jaime <jaimettb@gmail.com>
2025-06-29 22:45:29 -0300

    Async and loader for movie list

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -55,11 +55,20 @@ const KEY = "f84fc31d";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const query = "Matrix";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=matrix`)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+    async function fetchMovies() {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    }
+    fetchMovies();
   }, []);
 
   return (
@@ -69,9 +78,7 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <>
             <WhatchedSummary watched={watched} />
@@ -81,6 +88,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -17,8 +17,11 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode>
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+
+  // <React.StrictMode>
   //   <StarRating
   //     maxRating={5}
   //     messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
