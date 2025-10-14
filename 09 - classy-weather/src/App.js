01bc06cb324072ec13f4dd28488845eabe8f5dec
Jaime <jaimettb@gmail.com>
2025-10-14 09:57:34 -0300

    Handling events

--- 09 - classy-weather/src/App.js
+++ 09 - classy-weather/src/App.js
@@ -5,14 +5,35 @@ class Counter extends React.Component {
     super(props);
 
     this.state = { count: 5 };
+    this.hendleDecrement = this.hendleDecrement.bind(this);
+    this.hendleIncrement = this.hendleIncrement.bind(this);
+  }
+
+  hendleDecrement() {
+    console.log(this);
+    this.setState((curStarte) => {
+      return { count: curStarte.count - 1 };
+    });
+  }
+
+  hendleIncrement() {
+    console.log(this);
+    this.setState((curStarte) => {
+      return { count: curStarte.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.hendleDecrement}>-</button>
+        <span>
+          {date.toDateString()} {this.state.count}
+        </span>
+        <button onClick={this.hendleIncrement}>+</button>
       </div>
     );
   }
